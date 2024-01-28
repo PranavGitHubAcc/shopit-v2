55847cf0496f9e9edfbf1162315c0ec52a4c32e7
PranavGitHubAcc <pranavmahajan18@gmail.com>
2024-01-29 01:29:00 +0530

    .

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,29 @@
+import "./App.css";
+
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+
+import Home from "./components/Home.jsx";
+
+
 import Footer from "./components/layout/Footer.jsx";
 import Header from "./components/layout/Header.jsx";
 
 function App() {
-    return (
-        <div className="App">
-            <Header />
-            <Footer />
-        </div>
-    );
+  return (
+      <Router>
+          <div className="App">
+              <Header />
+
+              <div className="container">
+                  <Routes>
+                      <Route path="/" element={<Home />}></Route>
+                  </Routes>
+              </div>
+
+              <Footer />
+          </div>
+      </Router>
+  );
 }
 
 export default App;
