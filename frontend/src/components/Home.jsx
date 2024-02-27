7f773796cb2ac7f125c4ddd09904af63b0e908da
PranavGitHubAcc <pranavmahajan18@gmail.com>
2024-02-27 08:49:05 +0530

    Adding min-max Filters

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -6,14 +6,18 @@ import Loader from "./layout/Loader";
 import toast from "react-hot-toast";
 import CustomPagination from "./layout/CustomPagination";
 import { useSearchParams } from "react-router-dom";
+import Filters from "./layout/Filters";
 
 const Home = () => {
     let [searchParams] = useSearchParams();
     const page = searchParams.get("page") || 1;
+    const keyword = searchParams.get("keyword") || "";
 
-    const params = { page };
+    const params = { page, keyword };
 
     const { data, isLoading, error, isError } = useGetProductsQuery(params);
+
+    const columnSize = keyword ? 4 : 3;
 
     useEffect(() => {
         if (isError) {
@@ -25,19 +29,31 @@ const Home = () => {
     return (
         <>
             <MetaData title={"Buy Best Products Online"} />
-            <div className="col-6 col-md-12">
-                <h1 id="products_heading" className="text-secondary">
-                    Latest Products
-                </h1>
-                <section id="products" className="mt-5">
-                    <div className="row">
+            <div className="row">
+                {keyword && (
+                    <div className="col-6 col-md-3 mt-5">
+                        <Filters />
+                    </div>
+                )}
+                <div className={keyword ? "col-6 col-md-9" : "col-6 col-md-12"}>
+                    <h1 id="products_heading" className="text-secondary">
+                        {keyword
+                            ? `${data?.products?.length} products found with keyword: "${keyword}"`
+                            : "Latest Products"}
+                    </h1>
+
+                    <section id="products" className="mt-5">
                         <div className="row">
                             {data?.products?.map((product) => (
-                                <ProductItem product={product} />
+                                <ProductItem
+                                    key={product._id}
+                                    product={product}
+                                    columnSize={columnSize}
+                                />
                             ))}
                         </div>
-                    </div>
-                </section>
+                    </section>
+                </div>
             </div>
             <CustomPagination
                 resPerPage={data?.resPerPage}
